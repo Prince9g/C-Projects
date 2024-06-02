72cb0eb5c13bb18c0cc0807544455240cb86b87c
Prince9g <142648202+Prince9g@users.noreply.github.com>
2024-06-02 07:03:32 +0530

    do some known fixes

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -5,6 +5,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int ntail;
 enum eDirection {Stop = 0, Left, Right, up, down};
 eDirection dir;
 void setup(){
@@ -17,9 +19,10 @@ void setup(){
     score = 0;
 }
 void draw(){
+    cout << x;
     system("cls");
     for(int i = 0; i <= width; i++){
-        cout << '#';
+        cout << '-';
     }
     cout << endl;
     for(int i = 0; i<height; i++){
@@ -34,7 +37,16 @@ void draw(){
                 cout << "F";
             }
             else{
-                cout << " ";
+                bool print = false;
+                for(int k=0; k<ntail; k++){
+                    if(tailY[k] == i && tailX[k] == j){
+                        print = true;
+                        cout << 'o';
+                    }
+                }
+                if(!print){
+                    cout << " ";
+                }
             }
         }
         cout << endl;
@@ -67,6 +79,19 @@ void input(){
     }
 }
 void logic(){
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2x, prev2y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i=0; i<ntail; i++){
+        prev2x = tailX[i];
+        prev2y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2x;
+        prevY = prev2y;
+    }
     switch(dir){
         case Left:
             x--;
@@ -75,10 +100,10 @@ void logic(){
             x++;
             break;
         case up:
-            y++;
+            y--;
             break;
         case down:
-            y--;
+            y++;
             break;
         default:
             break;
@@ -87,14 +112,20 @@ void logic(){
     if(x > width || x < 0 || y > height || y < 0){
         gameOver = true;
     }
-    
+    for(int i=0; i<ntail; i++){
+        if(tailX[i] == x && tailY[i] == y){
+            gameOver = true;
+        }
+    }
     if(x == fruitX && y == fruitY){
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
+        ntail++;
     }
 }
 int main(){
+    setup();
     while(!gameOver){
         draw();
         input();
